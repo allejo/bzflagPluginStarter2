18677374db2f811c13ac2c0de828843e4629d23d
allejo <allejo@me.com>
2017-01-05 18:55:34 -0800

    Get a baseurl working

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,10 @@
 var bzEvents = {};
 var licenses = {};
 $.when(
-    $.ajax('/data/events.json').done(function (data) {
+    $.ajax('/bzflagPluginStarter2/data/events.json').done(function (data) {
         bzEvents = data;
     }),
-    $.ajax('/data/licenses.json').done(function (data) {
+    $.ajax('/bzflagPluginStarter2/data/licenses.json').done(function (data) {
         licenses = data;
     })
 ).then(function () {
