c01bf3c3a569bb87f8232484da37536d6f5cbb9a
allejo <allejo@me.com>
2017-05-07 01:18:00 -0700

    Add Makefile to ignore + copy events differently

--- tools/fetchEvents.sh
+++ tools/fetchEvents.sh
@@ -4,6 +4,6 @@ mkdir workspace
 git clone https://github.com/bzflag-dev/bzflag.org workspace/bzflag.org/
 
 mkdir _events/
-cp -R workspace/bzflag.org/_documentation/developer/bzfs_api_events/ _events/
+cp -a workspace/bzflag.org/_documentation/developer/bzfs_api_events/. _events/
 
 rm -rf workspace/
