272cb6ec32a7b38203cc9b1fcf2711482daeb2d9
allejo <allejo@me.com>
2018-04-08 00:35:06 -0700

    Use GitHub API to fetch bzfs event data
    Netlify had issues supporting nodegit bindings, so I abandoned that to
    avoid headaches. Downloading a Zip of the repo and unzipping it copying
    files was too much of a pain. So... Let's just manually use the GH API
    because fuck it.

--- scripts/fetchEvents.js
+++ scripts/fetchEvents.js
@@ -1,36 +1,34 @@
 #!/usr/bin/env node
 
-const fs = require('fs-extra');
-const git = require('nodegit');
+const octokit = require('@octokit/rest')();
+const http = require('https');
 const path = require('path');
+const fs = require('fs');
 
 // Location for event docs to go
 const eventsFolder = path.join(__dirname, '..', 'src', 'events');
 
-async function process() {
-    try {
-        // Remove the old workspace, if it exists
-        await fs.remove('workspace');
+octokit.repos
+    .getContent({
+        owner: 'bzflag-dev',
+        repo: 'bzflag.org',
+        path: '_documentation/developer/bzfs_api_events'
+    })
+    .then(function(result) {
+        result.data.forEach(function(value) {
+            const filename = value.name;
 
-        // Initialize a new workspace directory
-        fs.mkdirp('workspace');
-
-        // Clone bzflag.org to the workspace
-        const repo = await git.Clone('https://github.com/bzflag-dev/bzflag.org', 'workspace/bzflag.org/');
-        const commit = await repo.getMasterCommit();
-
-        console.log(`Using API events from commit: ${commit.toString().slice(-7)}`);
-
-        // Remove any markdown files that may exist so we only use what we copy from the repo
-        fs.remove(path.join(eventsFolder, '*.md'));
-
-        // Move what we cloned over to the events folder
-        await fs.copy('workspace/bzflag.org/_documentation/developer/bzfs_api_events/', eventsFolder, {
-            dereference: true
+            http.get(value.download_url, function(res) {
+                res.on('data', function(chunk) {
+                    fs.writeFile(path.join(eventsFolder, filename), chunk, function(err) {
+                        if (err) {
+                            console.error(err);
+                        }
+                    });
+                });
+            });
         });
-    } catch (error) {
+    })
+    .catch(function(error) {
         console.error(error);
-    }
-}
-
-process();
+    });
