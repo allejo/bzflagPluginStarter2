241a92ddc4ace4eb62f67dcc01d83cda8d93f958
Vladimir Jimenez <allejo@me.com>
2018-12-18 00:23:00 -0800

    Simplify the NameChunk writer unit tests

--- src/lib/PluginBuilder/ChunkWriter/__tests__/NameChunk.test.ts
+++ src/lib/PluginBuilder/ChunkWriter/__tests__/NameChunk.test.ts
@@ -15,10 +15,9 @@ test('NameChunk creates Name() method in given CPPClass instance', () => {
     const chunk = new NameChunk(pluginClass, pluginDef.definition);
     chunk.process();
 
-    const methods = pluginClass.getMethods();
-    const methodSigs = Object.keys(methods);
+    const methods = Object.keys(pluginClass.getMethods());
 
-    expect(methodSigs).toContain('const char* Name()');
+    expect(methods).toContain(chunk.getIdentifier());
 });
 
 test('NameChunk creates Name() method returning human-friendly name', () => {
@@ -27,10 +26,8 @@ test('NameChunk creates Name() method returning human-friendly name', () => {
     const chunk = new NameChunk(pluginClass, pluginDef.definition);
     chunk.process();
 
-    const methods = pluginClass.getMethods();
-    const nameFxn = methods['const char* Name()'];
-
-    const output = nameFxn.functionDef.write(codeStyle, 0);
+    const method = pluginClass.getMethods()[chunk.getIdentifier()];
+    const output = method.functionDef.write(codeStyle, 0);
 
     expect(output).toContain(`return "${pluginDef.definition.name}";`);
 });
@@ -39,10 +36,8 @@ test('NameChunk creates Name() method with correct code', () => {
     const chunk = new NameChunk(pluginClass, pluginDef.definition);
     chunk.process();
 
-    const methods = pluginClass.getMethods();
-    const nameFxn = methods['const char* Name()'];
-
-    const output = nameFxn.functionDef.write(codeStyle, 0);
+    const method = pluginClass.getMethods()[chunk.getIdentifier()];
+    const output = method.functionDef.write(codeStyle, 0);
     const expected = multiLineString(`
 const char* TestClass::Name()
 {
